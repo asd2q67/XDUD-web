31d7a18dd58ec6078133cebbc30fa028a2c6799c
HaoPro <haopka.cnttvj@gmail.com>
2022-12-12 18:32:25 +0700

    update side pages add main page

---
+++ scripts/main-page.js
@@ -0,0 +1,29 @@
+// NavBar
+window.addEventListener('scroll',function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('scroll-change',window.scrollY > 0)
+});
+//Filter Cards
+// let list = document.querySelectorAll('.list');
+// let card = document.querySelectorAll('.card');
+
+// for(let i = 0; i<list.length; i++){
+//     list[i].addEventListener('click',function(){
+//         for(let j=0; j<list.length; j++){
+//             list[j].classList.remove('active');
+//         }
+//         this.classList.add('active');
+
+//         let dataFilter = this.getAttribute('data-filter');
+
+//         for(let k=0; k<card.length;k++){
+//             card[k].classList.remove('active');
+//             card[k].classList.add('hide');
+
+//             if(card[k].getAttribute('data-item') == dataFilter || dataFilter == 'all'){
+//                 card[k].classList.remove('hide');
+//                 card[k].classList.add('active');
+//             }
+//         }
+//     });
+// }
