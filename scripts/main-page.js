b4f94c17667920a63fc6b35202fdfcd9943bf7c1
CongProVjp <20010938@st.phenikaa-uni.edu.vn>
2022-12-13 17:33:21 +0700

    transporting update

--- scripts/main-page.js
+++ scripts/main-page.js
@@ -1,4 +1,4 @@
-filterSelection('all')
+
 
 function filterSelection(c) {
     console.log(c);
@@ -34,9 +34,52 @@ function removeCard(element,name){
     for (i=0; i < arr2.length; i++){
         while(arr1.indexOf(arr2[i]) > -1)
         {
-            arr1.splice(arr1.indexOf(arr2[i]), 1)
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
         }
     }
 
     element.className = arr1.join(" ");
 }
+
+/*to top button*/
+
+//get the button
+let topBtn = document.getElementsByClassName("aButton");
+
+//show the button when user scroll down
+
+
+window.onscroll = function()
+{
+    console.log(document.documentElement.scrollTop)
+}
+
+
+function toTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function toAboutUs(){
+    document.body.scrollTop = 650;
+    document.documentElement.scrollTop = 650;
+}
+
+function toGames(){
+    document.body.scrollTop = 1195;
+    document.documentElement.scrollTop = 1195;
+}
+
+function toTournament(){
+    document.body.scrollTop = 2480;
+    document.documentElement.scrollTop = 2480;
+}
+
+function toContact(){
+    document.body.scrollTop = 3502;
+    document.documentElement.scrollTop = 3502;
+}
+
+
+
+//console.log(document.documentElement.scrollTop);
